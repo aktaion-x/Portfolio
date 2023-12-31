537d23052d7b14ae9e4f1f3f8f39fcb95fef3f9f
aktaion-x@aktaion <aktaion-x@proton.me>
2023-12-31 12:42:58 +0300

    drop the api

--- api/src/middleware/multer.ts
+++
@@ -1,9 +0,0 @@
-import multer from "multer"
-
-const storage = multer.diskStorage({
-  filename: function (req: Express.Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) {
-    cb(null, file.originalname);
-  }
-});
-const upload = multer({ storage: storage });
-export default upload;
--- api/src/routes/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { createUser, loginUser } from '../controllers/user.controller'
-const router = express.Router();
-
-router.post('/login', loginUser)
-// disable creating new users
-// router.post('/create', createUser)
-
-export default router;
